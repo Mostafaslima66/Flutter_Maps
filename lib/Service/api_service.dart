3bc65ecb6f3db4ae7cbeac8ffb1fe9a4b6983f79
Mostafa_Slima <150176123+Mostafaslima66@users.noreply.github.com>
2024-06-30 19:21:24 +0300

    Update api_service.dart

--- lib/Service/api_service.dart
+++ lib/Service/api_service.dart
@@ -1,9 +1,9 @@
 Uri getGeocodeUrl(String query) {
   return Uri.parse(
-      'https://api.geoapify.com/v1/geocode/search?text=$query&apiKey=5b3ce3597851110001cf6248450a76d8a03f41e8b7cecefcdd86d6ec');
+      'https://api.geoapify.com/v1/geocode/search?text=$query&apiKey='Write your API key');
 }
 
 Uri getRouteUrl(String start, String end) {
   return Uri.parse(
-      'https://api.geoapify.com/v1/routing?waypoints=$start|$end&mode=drive&apiKey=5b3ce3597851110001cf6248450a76d8a03f41e8b7cecefcdd86d6ec');
+      'https://api.geoapify.com/v1/routing?waypoints=$start|$end&mode=drive&apiKey='Write your API key');
 }
